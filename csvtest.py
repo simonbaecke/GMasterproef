7301baba3117130bec6ffa75d2fc188c187efbd2
simonbaecke <simon.baecke@gmail.com>
2023-02-19 11:54:56 +0100

    added files

--- csvtest.py
+++ csvtest.py
@@ -1,6 +1,18 @@
 from urllib.request import urlopen
+import csv
+import pandas as pd
 
 url = "https://raw.githubusercontent.com/simonbaecke/Masterproef/main/csvtest.csv"
-invoer = urlopen(url)
-data = invoer.read().decode('utf-8')
-print(data)
+dataframe=pd.read_csv(url)
+print(dataframe)
+
+
+dataframe.set_index('id',inplace=True)
+valueneeded = dataframe.loc['a','value']
+print(valueneeded)
+
+
+
+
+# invoer = urlopen(url)
+# data = invoer.read().decode('utf-8')
