a70426108e787994ea0529ceb8f1626ff10fce34
simonbaecke <simon.baecke@gmail.com>
2023-02-20 18:36:39 +0100

    csv data added

--- csvtest.py
+++ csvtest.py
@@ -2,14 +2,18 @@ from urllib.request import urlopen
 import csv
 import pandas as pd
 
-url = "https://raw.githubusercontent.com/simonbaecke/Masterproef/main/csvtest_p2.csv"
+url = "https://raw.githubusercontent.com/simonbaecke/Masterproef/main/csv%20p2.csv"
 dataframe=pd.read_csv(url)
 print(dataframe)
 
 
 dataframe.set_index('id',inplace=True)
-valueneeded = dataframe.loc['a','value']
-print(valueneeded)
+empty = {}
+for letter in 'abcdef':
+    print(letter)
+    empty[letter]=dataframe.loc[letter,'value']
+a = dataframe.loc['a','value']
+print(empty)
 
 
 
