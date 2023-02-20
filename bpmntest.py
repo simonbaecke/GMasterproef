caf5bfe1bbaf92ee285aa89fc50d36d882d027e6
simonbaecke <simon.baecke@gmail.com>
2023-02-20 14:31:46 +0100

    idk

--- bpmntest.py
+++ bpmntest.py
@@ -13,12 +13,31 @@ from bs4 import BeautifulSoup
 
 bestand = open('diagram.bpmn','r').read()
 bs_data=BeautifulSoup(bestand,'xml')
-b_name = bs_data.find_all('task')
-for task in b_name:
-    print(type(task))
-    print(task.get('name'))
+start = bs_data.startEvent
+print(start['name'])
 
-b_data=bs_data.find('dataObjectReference')
+start_outgoingstring = start.outgoing.text
+print(start_outgoingstring)
+
+next = bs_data.find_all(string=start_outgoingstring)
+inenuit = []
+
+for tag in next:
+    # print(tag.parent.parent.prettify())
+    inenuit.append(tag.parent.parent)
+# print(inenuit)
+for tag in inenuit:
+    a = tag.find('incoming',string=start_outgoingstring)
+    if a is not None:
+        print(a.prettify())
+    # if a.string == start_outgoingstring:
+    #     print('yes')
+# b_name = bs_data.find_all('task')
+# for task in b_name:
+#     print(type(task))
+#     print(task.get('name'))
+
+# b_data=bs_data.find('dataObjectReference')
 
 
 # b_idk = bs_data.find('task')
