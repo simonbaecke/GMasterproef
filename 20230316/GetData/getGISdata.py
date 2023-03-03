789d213175f89cfb527c0569abec3f325580793a
simonbaecke <simon.baecke@gmail.com>
2023-03-03 13:42:27 +0100

    new session

---
+++ 20230316/GetData/getGISdata.py
@@ -0,0 +1,35 @@
+import requests
+import urllib.parse
+import json
+import getcoordinates
+
+# x = float(input())
+# y = float(input())
+# x = getcoordinates.x
+# y = getcoordinates.y
+x=100688
+y=207134
+
+percentencodedxy = urllib.parse.quote(str(x)+' '+str(y))
+urlperceel = "https://geo.api.vlaanderen.be/GRB/wfs?service=WFS&version=2.0.0&request=GetFeature&typeNames=GRB:ADP&outputFormat=application/json&CQL_FILTER=CONTAINS(SHAPE,POINT(url))".replace('url',percentencodedxy)
+bestandsnaamperceel = 'perceel'
+urlwater = "https://www.dov.vlaanderen.be/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeNames=gw_bescherming:beschermingszones_2016&outputFormat=application/json&CQL_FILTER=CONTAINS(geom,POINT(url))".replace('url',percentencodedxy)
+bestandsnaamwater = 'winwatergebied'
+
+def getWFS(url,bestandsnaam):
+    user_agent = 'Mozilla/5.0'
+    response = requests.get(url, headers={'User-Agent': user_agent})
+
+    if response.status_code == 200:
+        data = response.json()
+        json_string = json.dumps(data, ensure_ascii=False, indent=4)
+        with open("Output/"+bestandsnaam+".json", "w") as write_file:
+            write_file.write(json_string)
+        return data
+    else:
+        response.raise_for_status()
+
+getWFS(urlperceel,bestandsnaamperceel)
+getWFS(urlwater,bestandsnaamwater)
+
+print('2 JSON files generated')
---
+++ 20230316/bpmn processor.py
@@ -0,0 +1,103 @@
+bpmn = "diagramv3.bpmn"
+import html
+from bs4 import BeautifulSoup
+from getCSV import idvalue
+print(idvalue) #check waarden van csv
+
+def convertformula(database,source):
+    formula = str(html.unescape(source))
+    i=1
+    while i < len(formula)-1:
+        if formula[i] in database.keys() and formula[i-1] not in database.keys()and formula[i+1] not in database.keys():
+            formula = formula[:i]+str(database[formula[i]])+formula[i+1:]
+        i+=1
+    if formula[0] in database.keys() and formula[1] not in database.keys():
+        formula = str(database[formula[0]])+formula[1:]
+    if formula[len(formula)-1] in database.keys() and formula[len(formula)-2] not in database.keys():
+        formula = formula[:len(formula)-1]+str(database[formula[len(formula)-1]])
+    
+    return formula
+
+bestand = open(bpmn,'r').read()
+bsdata=BeautifulSoup(bestand,'xml')
+data = bsdata.process
+graphics=bsdata.BPMNPlane
+start = data.startEvent
+
+shapecolorid=[start['id']]
+linecolorid=[]
+wrongoutputlineid=[]
+
+next = start
+while next.name != "endEvent":
+    for content in data:
+        try:
+            incoming = content.find_all('incoming')
+            all_incoming = [x.string for x in incoming]
+            if next.outgoing.string in all_incoming:                
+                next = content
+                shapecolorid.append(next['id'])
+
+                #bekijken of het een gateway is, anders gewoon doorgaan
+                while next.name in ["exclusiveGateway","intermediateThrowEvent"]:
+                    nota = data.find('association',attrs = {'sourceRef' : next['id']})
+                    noto=data.find('textAnnotation',attrs = {'id' : nota['targetRef']})
+                    shapecolorid.append(noto['id'])
+                    print(noto.text)
+                    idgateway=next['id']
+                    formula = convertformula(idvalue,noto.text)
+                    if next.name == "exclusiveGateway":
+                        answer = str(eval(formula))
+                        for flow in data.find_all('sequenceFlow',attrs = {'sourceRef' : idgateway}):
+                            if flow['name'] == answer:
+                                nextflow = flow
+                            else:
+                                wrongoutputlineid.append(flow['id'])
+                        idtarget=nextflow['targetRef']
+                        next = data.find(attrs = {'id' : idtarget})
+                        shapecolorid.append(next['id'])
+                    else:
+                        exec(formula)
+                        idvalue[formula[0]]=eval(formula[0])
+                        break
+                        
+                
+                #nieuwe parameters maken en toevoegen aan idvalue
+                # if next.name == "intermediateThrowEvent":
+                #     formula = convertformula(idvalue,next['name'])
+                    # exec(formula)
+                    # idvalue[formula[0]]=eval(formula[0])
+
+        except:
+            pass
+print(next.prettify())
+
+#kleuren van tasks die niet uitgevoerd worden
+for content in data:
+    try:
+        if content['sourceRef'] in shapecolorid and content['targetRef'] in shapecolorid:
+            linecolorid.append(content['id'])
+    except:
+        pass
+linecolorids = [x for x in linecolorid if x not in wrongoutputlineid]
+
+for shape in graphics:
+    try:
+        id = shape['bpmnElement']
+        if shape.name == "BPMNShape" and id not in shapecolorid and id[0:19] != "DataObjectReference":
+            shape['bioc:stroke']="#B7B7B7"
+            # shape["bioc:fill"]="#F8F8F8"
+            # shape["color:background-color"]="#F8F8F8"
+            shape["color:border-color"]="#B7B7B7"
+            daaa='d'
+        elif shape.name == "BPMNEdge"and id not in linecolorids and id[0:20] != "DataInputAssociation":
+            shape['bioc:stroke']="#B7B7B7"
+            shape["color:border-color"]="#B7B7B7"
+    except:
+        pass
+
+# exporteren van aangepaste bpmn
+with open("./Output/edited.bpmn","w") as new:
+    new.write(str(bsdata))
+
+print(idvalue)
